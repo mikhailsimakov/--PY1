d9014e51fed59bd910ba53ccd00ebd31d7fed7d1
mikhailsimakov <115424657+mikhailsimakov@users.noreply.github.com>
2022-10-16 22:27:52 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+# True and True or False and False
+# True or False
+
+
+result = True  # TODO подставить результат выражения
+
+print(src == result)
